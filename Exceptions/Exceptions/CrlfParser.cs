6a7f4d029e28a24de169f3219a6236b7136ef7ba
Jim Counts <jamesrcounts@outlook.com>
2013-10-08 21:30:39 -0700

    Testing with Expected Exception

---
+++ Exceptions/Exceptions/CrlfParser.cs
@@ -0,0 +1,16 @@
+﻿namespace Exceptions
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using Newtonsoft.Json;
+
+    public static class CrlfParser
+    {
+        public static dynamic DeserializeJson(string source)
+        {
+            var text = Regex.Replace(source, @"\r?\n", Environment.NewLine);
+            var o = JsonConvert.DeserializeObject(text);
+            return o;
+        }
+    }
+}
---
+++ Exceptions/Exceptions/JsonParserTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Exceptions
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class JsonParserTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ShouldValidateSource()
+        {
+            CrlfParser.DeserializeJson(null);
+        }
+    }
+}
