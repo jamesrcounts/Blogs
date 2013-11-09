dfe0b4b4502c3df4d527023692b8492c607bb7e1
Jim Counts <jamesrcounts@outlook.com>
2013-11-09 15:55:03 -0800

    Temporary File Class

---
+++ NugetLikeAPro/TemporaryFile.Tests/TempTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemporaryFile.Tests
+{
+    using System.IO;
+
+    using Xunit;
+
+    public class TempTest
+    {
+        [Fact]
+        public void DeleteBackingFile()
+        {
+            var temp = new Temp("DeleteBackingFile.txt");
+            Assert.True(temp.File.Exists);
+            temp.Dispose();
+            Assert.False(File.Exists(temp.File.FullName));
+        }
+    }
+}
---
+++ NugetLikeAPro/TemporaryFile/TemporaryFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TemporaryFile
+{
+    using System.IO;
+
+    using ApprovalUtilities.Utilities;
+
+    public class Temp : IDisposable
+    {
+        private readonly FileInfo backingFile;
+
+        public Temp(string name)
+        {
+            this.backingFile = new FileInfo(PathUtilities.GetAdjacentFile(name));
+            this.backingFile.Create().Close();
+        }
+
+        ~Temp()
+        {
+            this.Dispose();
+        }
+
+        public FileInfo File
+        {
+            get
+            {
+                return this.backingFile;
+            }
+        }
+
+        public void Dispose()
+        {
+            // File on the file system is not a managed resource :)
+            if (this.backingFile.Exists)
+            {
+                this.backingFile.Delete();
+            }
+        }
+    }
+}
