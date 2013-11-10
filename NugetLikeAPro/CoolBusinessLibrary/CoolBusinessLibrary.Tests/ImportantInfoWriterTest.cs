4b17d87ecf441bea4d75ef68d68a0616b48b27f8
Jim Counts <jamesrcounts@outlook.com>
2013-11-10 10:44:10 -0800

    Cool Business Library

---
+++ NugetLikeAPro/CoolBusinessLibrary/CoolBusinessLibrary.Tests/ImportantInfoWriterTest.cs
@@ -0,0 +1,22 @@
+﻿namespace CoolBusinessLibrary.Tests
+{
+    using System.IO;
+
+    using TemporaryFile;
+
+    using Xunit;
+
+    public class ImportantInfoWriterTest
+    {
+        [Fact]
+        public void WriteImportantInfo()
+        {
+            using (var tf = new Temp("info.txt"))
+            {
+                ImportantInfoWriter.WriteTo(tf.File);
+                string actual = File.ReadAllText(tf.File.FullName);
+                Assert.Equal("This information is very important!", actual);
+            }
+        }
+    }
+}
---
+++ NugetLikeAPro/CoolBusinessLibrary/CoolBusinessLibrary/ImportantInfoWriter.cs
@@ -0,0 +1,15 @@
+﻿namespace CoolBusinessLibrary
+{
+    using System.IO;
+
+    public class ImportantInfoWriter
+    {
+        public static void WriteTo(FileInfo file)
+        {
+            using (var w = new StreamWriter(file.OpenWrite()))
+            {
+                w.Write("This information is very important!");
+            }
+        }
+    }
+}
