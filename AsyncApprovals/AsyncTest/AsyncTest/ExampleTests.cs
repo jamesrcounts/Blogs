94fc21f3b42c48f083cceb1dcedddef076185963
Jim Counts <jamesrcounts@outlook.com>
2014-04-12 14:06:23 -0700

    Created project

---
+++ AsyncApprovals/AsyncTest/AsyncTest/ExampleTests.cs
@@ -0,0 +1,15 @@
+﻿namespace AsyncTest
+{
+    using System.Net.Http;
+
+    using Xunit;
+
+    public class ExampleTests
+    {
+        [Fact]
+        public void TypicalAsyncTest()
+        {
+            var httpClient = new HttpClient();
+        }
+    }
+}
