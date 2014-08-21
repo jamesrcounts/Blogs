0f5932fa05673229c1e5c86cfcb422d72bb3d2bf
Jim Counts <jamesrcounts@outlook.com>
2014-08-20 22:26:23 -0700

    Property Based Test With AutoFixture and Xunit

---
+++ PropertyBasedTests/PropertyBasedTests/PropertyTest.cs
@@ -0,0 +1,42 @@
+﻿namespace PropertyBasedTests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Ploeh.AutoFixture;
+
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class PropertyTest
+    {
+        private static readonly Fixture Fixture = new Fixture();
+
+        public static IEnumerable<object[]> EvenNumbers
+        {
+            get
+            {
+                return Numbers().Take(100).Select(n => new object[] { n });
+            }
+        }
+
+        [Theory]
+        [PropertyData("EvenNumbers")]
+        public void EvenOddTheory(int n)
+        {
+            Assert.True((n + 1) % 2 == 1, n + " + 1 is odd");
+        }
+
+        private static IEnumerable<int> Numbers()
+        {
+            while (true)
+            {
+                var i = Fixture.Create<int>();
+                if (i % 2 == 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
